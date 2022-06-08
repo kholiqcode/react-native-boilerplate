07498eb33232b3bc22a80aa22d09f59e9fb821ca
Kevin <44430202+venxik@users.noreply.github.com>
2022-06-08 22:22:29 +0700

    feat(conventionalCommit): add conventional commits

---
+++ __mocks__/reactotron-react-native.js
@@ -0,0 +1,10 @@
+const reactotron = {
+  configure: () => reactotron,
+  useReactNative: () => reactotron,
+  use: () => reactotron,
+  connect: () => reactotron,
+  setAsyncStorageHandler: () => reactotron,
+  createEnhancer: () => reactotron,
+};
+
+module.exports = reactotron;
